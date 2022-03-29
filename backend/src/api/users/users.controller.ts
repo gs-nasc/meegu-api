b0c984ffac30f99fae9cdbd89ba477d17b24e302
Gabriel Nascimento <desenvolvimento@vipautomacao.com.br>
2022-03-29 13:13:53 -0300

    User controller ready

--- backend/src/api/users/users.controller.ts
+++ backend/src/api/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query, UseGuards } from '@nestjs/common';
+
+import { User } from './interfaces/user.interface';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private usersService: UsersService) { }
+
+    @Get()
+    async findAll(): Promise<User[]> {
+        return await this.usersService.findAll();
+    }
+
+    @Get(":id")
+    async findById(@Param('id', ParseIntPipe) id: number): Promise<User> {
+        return await this.usersService.findById(id);
+    }
+
+    @Post()
+    async create(@Body() user: User): Promise<User> {
+        return await this.usersService.create(user);
+    }
+
+    @Put(":id")
+    async update(@Param('id', ParseIntPipe) id: number, @Body() user: User): Promise<User> {
+        return await this.usersService.update(id, user);
+    }
+
+    @Get("?")
+    async findByName(@Query("name") name: string): Promise<User> {
+        return await this.usersService.findByName(name);
+    }
+
+    @Delete(":id")
+    async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return await this.usersService.delete(id);
+    }
+}
