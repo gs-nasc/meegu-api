1c1f0875a1ea75784a4cf1c93391ac2e3ac54d71
Gabriel Nascimento <desenvolvimento@vipautomacao.com.br>
2022-03-29 13:16:47 -0300

    Interceptor created

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './shared/interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: true
+  });
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+  });
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+  
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
