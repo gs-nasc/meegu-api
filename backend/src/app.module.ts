f9e9e75acfb0b07c68c79693b9f41584e53449e1
Gabriel Nascimento <desenvolvimento@vipautomacao.com.br>
2022-03-29 11:15:30 -0300

    Users initialized

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './api/users/users.controller';
+import { UsersService } from './api/users/users.service';
+import { UsersModule } from './api/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
